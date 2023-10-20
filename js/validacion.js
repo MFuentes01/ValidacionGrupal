15eabd89a4ed5d305b9d1ee20e377d7b9cf864b3
MFuentes01 <megax.yt.uy@gmail.com>
2023-10-20 10:47:01 -0300

    pruebas

--- js/validacion.js
+++ js/validacion.js
@@ -7,40 +7,40 @@ document.addEventListener('DOMContentLoaded', function () {
     let password2 = "";
 
     // Agregar evento 'input' para validar en tiempo real
-    campoPassword.addEventListener("input", passwordCheck);
-    campoPassword2.addEventListener("input", passwordCheck);
+    campoPassword.addEventListener("input", passwordCheck());
+    campoPassword2.addEventListener("input", passwordCheck());
 
     function passwordCheck() {
-        password = campoPassword.value;
-        password2 = campoPassword2.value;
 
-        if (password.length < 6 || password !== password2) {
-            campoPassword2.classList.remove('is-valid');
-            campoPassword2.classList.add('is-invalid');
-        } else {
+        if (campoPassword.value === campoPassword.value && campoPassword.value.length >= 6) {
             campoPassword2.classList.remove('is-invalid');
             campoPassword2.classList.add('is-valid');
-        }
-    }
+        } else {
+            campoPassword2.classList.remove('is-valid');
+            campoPassword2.classList.add('is-invalid');
+        };
+    };
 
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function () {
         'use strict'
-
+      
         // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        let forms = document.querySelectorAll('.needs-validation')
-
+        var forms = document.querySelectorAll('.needs-validation')
+      
         // Loop over them and prevent submission
-        Array.prototype.slice.call(forms).forEach(function (form) {
+        Array.prototype.slice.call(forms)
+          .forEach(function (form) {
             form.addEventListener('submit', function (event) {
-                if (!form.checkValidity()) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    })();
+              if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+              }
+      
+              form.classList.add('was-validated')
+            }, false)
+          })
+      })()
 });
 
 
