8d8d0f1f0cc119eb60c10af30a35daf480159b4a
facundodipaulo <141045729+facundodipaulo@users.noreply.github.com>
2023-10-20 10:54:10 -0300

    contraseña

--- js/validacion.js
+++ js/validacion.js
@@ -1,25 +1,20 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const campoPassword = document.getElementById("password1");
-    const campoPassword2 = document.getElementById("password2");
-    const registroBtn = document.getElementById("registrarse");
+  const campoPassword = document.getElementById("password1");
+  const campoPassword2 = document.getElementById("password2");
+  const registroBtn = document.getElementById("registrarse");
 
-    let password = "";
-    let password2 = "";
+  campoPassword.addEventListener("input", passwordCheck);
+  campoPassword2.addEventListener("input", passwordCheck);
 
-    // Agregar evento 'input' para validar en tiempo real
-    campoPassword.addEventListener("input", passwordCheck());
-    campoPassword2.addEventListener("input", passwordCheck());
-
-    function passwordCheck() {
-
-        if (campoPassword.value === campoPassword.value && campoPassword.value.length >= 6) {
-            campoPassword2.classList.remove('is-invalid');
-            campoPassword2.classList.add('is-valid');
-        } else {
-            campoPassword2.classList.remove('is-valid');
-            campoPassword2.classList.add('is-invalid');
-        };
-    };
+  function passwordCheck() {
+      if (campoPassword.value === campoPassword2.value && campoPassword.value.length >= 6) {
+          campoPassword2.classList.remove('is-invalid');
+          campoPassword2.classList.add('is-valid');
+      } else {
+          campoPassword2.classList.remove('is-valid');
+          campoPassword2.classList.add('is-invalid');
+      }
+  }
 
     // Example starter JavaScript for disabling form submissions if there are invalid fields
     (function () {
